49cb196e9f7d523205dc8ee651a02bb86a5e4ec2
Tony Cetuk <cetukt@gmail.com>
2018-01-11 23:55:30 -0500

    Added webpack server

--- src/app.js
+++ src/app.js
@@ -1,12 +1,52 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const template = (
-  <div>
-    <h2>Akira Project</h2>
-    <p>Welcome to the Akira Project!</p>
-  </div>
-);
+let count = 0;
+let appData = {
+  counts: []
+};
+
+function addOne() {
+  count++;
+  appData.counts.push(count);
+  render();
+}
+
+function subtractOne() {
+  count--;
+  appData.counts.pop();
+  render();
+}
+
+function reset() {
+  count = 0;
+  appData.counts = [];
+  render();
+}
+
+function render() {
+  const template = (
+    <div>
+      <h2>Akira GPS Project</h2>
+      <p>Welcome to the Akira GPS Project!</p>
+      <h4>Count: {count}</h4>
+      {
+        appData.counts.map(itemCount => {
+          return (
+            <p>Item #{itemCount}</p>
+          )
+        })
+      }
+      <div id='buttonBar'>
+        <button onClick={addOne}>Add One</button>
+        <button onClick={subtractOne}>Subtract One</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+
+  ReactDOM.render(template, rootElement);
+}
 
 const rootElement = document.getElementById('app');
-ReactDOM.render(template, rootElement);
+render();
